47a46711cbab5edfe948b0f8291d2fa20f4bd829
1OrlandoCastillo <orlandocastillomaria26@gmail.com>
2025-06-22 17:40:22 -0600

    Actualizar Creador.js

--- plugins2/Creador.js
+++ plugins2/Creador.js
@@ -1,32 +1,46 @@
-const handler = async (m, { conn }) => {
-  const ownerNumber = "5212731590195@s.whatsapp.net"; // Número del creador (WhatsApp internacional)
-  const ownerName = "Orlando xz 🤖"; // Nombre visible del creador
+import fetch from 'node-fetch';
 
-  const messageText = `📞 *Contacto del Creador del Subbot:*
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+   await m.react('🍑');
 
-Si tienes dudas, preguntas o sugerencias sobre el funcionamiento de *SonicBot Subbot*, puedes contactar a su creador.
+    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+    let name = await conn.getName(who);
+    let edtr = `@${m.sender.split('@')[0]}`;
+    let username = await conn.getName(m.sender);
 
-📌 *Nombre:* Orlando 
-📌 *Número:* +52 1 273 159 0195
-💬 *Toca el contacto para enviarle un mensaje directo.`;
+    // VCARD
+    let list = [{
+        displayName: "Orlando-Ofc 🎩",
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN: Orlando-ᴏғɪᴄɪᴀʟ🍭\nitem1.TEL;waid=5212731590195:521273159 0195\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET: ninopina10@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/crowbot_wa\nitem3.X-ABLabel:Internet\nitem4.ADR:;; Nicaragua;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
+    }];
 
-  // Enviar contacto vCard
-  await conn.sendMessage(m.key.remoteJid, {
-    contacts: {
-      displayName: ownerName,
-      contacts: [
-        {
-          vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${ownerName}\nTEL;waid=${ownerNumber.split('@')[0]}:+${ownerNumber.split('@')[0]}\nEND:VCARD`
+    await conn.sendMessage(m.chat, {
+        contacts: {
+            displayName: `${list.length} Contacto`,
+            contacts: list
+        },
+        contextInfo: {
+            externalAdReply: {
+                showAdAttribution: true,
+                title: 'һ᥆ᥣᥲ s᥆ᥡ Orlando-᥆𝖿ᥴ ᥱᥣ mᥱȷ᥆r',
+                body: 'Orlando Castillo', // <--- Cambiado aquí
+                thumbnailUrl: 'https://files.catbox.moe/wd3wwq.jpg',
+                sourceUrl: 'https://wa.me/5212731590195?text=Vengo+Del+Comando+.owner',
+                mediaType: 1,
+                renderLargerThumbnail: true
+            }
         }
-      ]
-    }
-  });
+    }, {
+        quoted: m
+    });
 
-  // Enviar texto informativo
-  await conn.sendMessage(m.key.remoteJid, {
-    text: messageText
-  }, { quoted: m });
+    let txt = `el contacto de mi creador*`;
+
+    await conn.sendMessage(m.chat, { text: txt });
 };
 
-handler.command = /^(creador|creator|owner|dueño)$/i;
-module.exports = handler;
+handler.help = ['owner', 'creator'];
+handler.tags = ['main'];
+handler.command = /^(owner|creator|creador|dueño)$/i;
+
+export default handler;
